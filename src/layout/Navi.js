f50aa7d940a44a0dfe945f52763d9925490d617b
kimgaaaa <dl4525@naver.com>
2024-09-27 16:09:43 +0900

    first commit

---
+++ src/layout/Navi.js
@@ -0,0 +1,35 @@
+import React,{useContext} from 'react';
+import { NavLink} from 'react-router-dom';
+import {DataContext} from '../App';
+
+const Navi = () => {
+	const {data, loading}=useContext(DataContext);
+
+	if(loading){
+		return <h1 className='loading'> 데이터 로드 중 입니다</h1>
+	}
+	const categories = [...new Set(data.map((item)=>item.RCP_PAT2))];
+
+	const activeStyle={
+		color:'#ff0',
+		textShadow:'2px 2px 5px #f00'
+	}
+	return (
+		<nav className='nav'>
+			<div className='inner'>
+				<ul className='menu'>
+					<li><NavLink to="/" style={({isActive})=>(isActive?activeStyle:undefined )}>Home</NavLink></li>
+					{
+						categories.map((category) => (
+							<li key={category}>
+								<NavLink to={`category/${category}`} style={({isActive})=>(isActive?activeStyle:undefined )}>{category}</NavLink>
+							</li>
+						))
+					}
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navi;
